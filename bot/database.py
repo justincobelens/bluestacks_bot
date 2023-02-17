0816257a18b4c91306c9fa7e092c77e3831ab8e6
JustinMac <justincobelens@gmail.com>
2023-02-17 22:04:57 +0100

    base

---
+++ bot/database.py
@@ -0,0 +1,33 @@
+import sqlite3
+
+conn = sqlite3.connect("database.db")
+
+cursor = conn.cursor()
+
+def create_table():
+    cursor.execute('''
+            CREATE TABLE IF NOT EXISTS tasks (
+            id INTEGER PRIMARY KEY,
+            task_name TEXT,
+            task_description TEXT,
+            task_status TEXT
+            )
+            ''')
+
+    conn.commit()
+
+def add_task():
+    cursor.execute('''
+        INSERT INTO tasks (task_name, task_description, task_status)
+        VALUES (?, ?, ?)
+    ''', ('Task 1', 'Do task 1', 'incomplete'))
+
+    conn.commit()
+
+cursor.execute('SELECT * FROM tasks')
+rows = cursor.fetchall()
+
+for row in rows:
+    print(row)
+
+conn.close()
---
+++ bot/task_queue.py
@@ -0,0 +1,3 @@
+
+class TaskQueue:
+    pass

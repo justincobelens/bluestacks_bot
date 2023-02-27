9426d0c21e1c1ed54ee2e472033ca49553ebeae4
JustinWindows <justincobelens@gmail.com>
2023-02-27 15:27:24 +0000

    opens/closes bluestacks and connects with adb
    loops through all bluestacks instances

--- bot/main.py
+++ bot/main.py
@@ -24,26 +24,58 @@ class Main:
         phone.start_adb_server()
 
         for instance in instances:
+            if len(phone.devices()) != 0:
+                phone.disconnect_all()
+
             instance_title = instance['Name']  # e.g. BlueStacks App Player 1
-            instance_name = instance['InstanceName']  # e.g. nougat64 or nougat64_2
+            instance_name = instance['InstanceName']  # e.g. Nougat64 or Pie64_2
             instance_path = f"{const.instance_path(instance_name)}"
 
             # 1) CREATE BLUESTACKS WINDOW
+            # TODO
+            # add check for if multiple windows are open
+            # for now, only 1 window should be open at the time
+
             # open window
             window.open(instance_path)
 
+            # wait for process to start
+            window.wait_for_process(state='start')
+
             # wait till window is open
-            window.wait(instance_title)
+            window.wait_for_window(instance_title)
 
             # arrange window
             window.arrange(instance_title, [0, 0, 593, 1020])
 
-            # 2) CREATE ADB CONNECTION
+            # 2) CONNECT ADB
             # get adb port
             adb_port = bluestacks.get_adb_port(instance_name)
-            print(adb_port)
+            serial = f"localhost:{adb_port}"
 
-            sys.exit()
+            # connect device
+            phone.connect(serial)
+
+            # wait till device is booted
+            phone.wait_for_state('local', 'device')
+            phone.get_state()
+
+            # wait for home screen to load
+            phone.wait_for_homescreen(serial)
+
+            # 3) PERFORM TASKS
+
+            # 4) DISCONNECT ADB
+            phone.disconnect_all()
+
+            # 5) CLOSE BLUESTACKS WINDOW
+            window.close(instance_title)
+
+            # wait for process to stop
+            window.wait_for_process(state='stop')
+
+
+        phone.stop_adb_server()
 
 
 if __name__ == '__main__':
--- bot/window.py
+++ bot/window.py
@@ -1,11 +1,34 @@
+import os
+
 import pygetwindow
-import json
+import pyautogui
 import time
+import numpy
+import win32api
+import win32con
+import win32gui
 
-from subprocess import Popen
-from dataclasses import dataclass, field
+from subprocess import Popen, call, DEVNULL
 
-import const
+
+def left_click(hwnd, pos):
+    lParam = win32api.MAKELONG(pos[0], pos[1])
+
+    win32gui.SendMessage(hwnd, win32con.WM_LBUTTONDOWN, win32con.MK_LBUTTON, lParam)
+    win32gui.SendMessage(hwnd, win32con.WM_LBUTTONUP, 0, lParam)
+    time.sleep(0.1)
+
+
+def find_close_btn(region):
+    image = pyautogui.screenshot(region=region)
+
+    image = numpy.array(image, dtype=numpy.uint8)
+    pixel_color = [235, 85, 62]
+
+    for i in range(0, len(image), 5):
+        for j in range(0, len(image[i]), 5):
+            if list(image[i][j]) == pixel_color:
+                return j, i
 
 
 class Window:
@@ -16,8 +39,17 @@ class Window:
     def open(self, path: str):
         self.process = Popen(path)
 
-    def close(self):
-        pass
+    def close(self, instance_title):
+        process = call(f"TASKKILL /PID {self.process.pid}", stdout=DEVNULL)
+        time.sleep(0.5)
+        if self.is_window(f'Close {instance_title}'):
+            close_whndl = win32gui.FindWindowEx(0, 0, None, f'Close {instance_title}')
+        else:
+            close_whndl = win32gui.FindWindowEx(0, 0, None, f'BlueStacks Exit Window')
+
+        region = win32gui.GetWindowRect(close_whndl)
+        dx, dy = find_close_btn([*region])
+        left_click(close_whndl, (dx, dy))
 
     @staticmethod
     def is_window(instance_title: str):
@@ -26,12 +58,31 @@ class Window:
         else:
             return False
 
-    def wait(self, instance_title: str):
+    def wait_for_process(self, state: str):
+        for _ in range(0, 60):
+            process = self.in_process()
+
+            if state == 'start' and process is True:
+                return True
+            elif state == 'stop' and process is False:
+                return True
+
+            time.sleep(1)
+        return False
+
+    def wait_for_window(self, instance_title: str):
         for _ in range(0, 60):
             if self.is_window(instance_title):
-                return
-            else:
-                time.sleep(1)
+                return True
+            time.sleep(1)
+        return False
+
+    def in_process(self):
+        tasklist = os.popen("tasklist").read().split()
+        if str(self.process.pid) in tasklist:
+            return True
+        else:
+            return False
 
     @staticmethod
     def arrange(window_title, region: list):
@@ -42,3 +93,4 @@ class Window:
 
         # set size of window
         bluestacks_window.resizeTo(region[2], region[3])
+
